3de95265e0af70ef94e1de08fe0a3fda21da5d15
sanjaybrijesh209 <107564577+sanjaybrijesh209@users.noreply.github.com>
2022-08-12 13:20:03 -0400

    created

---
+++ js/source.js
@@ -0,0 +1,40 @@
+
+
+
+document.addEventListener("DOMContentLoaded",(ev) =>{
+    textbar();
+    var logo = document.querySelector(".logo").addEventListener("click",() =>{
+        
+        location.reload();
+        
+        
+    })
+ 
+})
+
+
+
+function textbar(){
+
+    var hiring = document.getElementById("Hiring");
+    hiring.addEventListener("change",(ev) =>{
+        ev.preventDefault();
+        var input = document.createElement("input");
+        input.setAttribute("type","text");
+        input.setAttribute("placeholder","fee/hr");
+       
+    
+        var btn = document.querySelector(".txtbar").innerHTML = "";
+        var btn = document.querySelector(".txtbar").appendChild(input);
+    })
+
+    var comment = document.querySelector("#comment");
+    comment.addEventListener("change",() =>{
+        document.querySelector(".txtbar").innerHTML = "";
+    })
+    
+    var question = document.querySelector("#question");
+    question.addEventListener("change",() =>{
+        document.querySelector(".txtbar").innerHTML = "";
+    })
+}
